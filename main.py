25850e9eb3954775b9bfdad56506f30e76343768
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-07-28 15:12:44 +0300

    QVBoxLayout added, pandas table added

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QTableWidget, QTableWidgetItem, QComboBox, QGridLayout, QLabel, QStackedWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QTableView, QComboBox, QGridLayout, QLabel, QStackedWidget
 from PyQt5.QtCore import QSize
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from csv_viewer import CSVViewer
+from pandas_model import PandasModel
 import numpy as np
-from scipy.stats import norm
+import pandas as pd
 
-
-class MainApp(QWidget): # Класс наследуется от QWidget и является основным классом приложения.
+class MainApp(QWidget): 
     def __init__(self):
         super().__init__()
 
-        self.viewer = CSVViewer() # Создаем экземпляр класса CSVViewer, который будет заниматься обработкой CSV файлов.
+        self.viewer = CSVViewer()
 
         self.initUI()
 
     def initUI(self):
-        self.setMinimumSize(QSize(960, 720))  # Задаем минимальный размер окна
+        self.setMinimumSize(QSize(960, 720))  
+
+        self.gaussian_data = pd.DataFrame(columns=['Reaction', 'Height', 'Center', 'Width'])
+        self.gaussian_model = PandasModel(self.gaussian_data)
+        self.gaussian_table = QTableView()
+        self.gaussian_table.setModel(self.gaussian_model)
+
+        self.csv_table = QTableView()
+
+        self.stacked_widget = QStackedWidget()
+        self.stacked_widget.addWidget(self.csv_table)
+        self.stacked_widget.addWidget(self.gaussian_table)
 
         self.buttonLoadCSV = QPushButton('Load CSV', self)
         self.buttonLoadCSV.clicked.connect(self.getCSV)
@@ -38,36 +49,45 @@ class MainApp(QWidget): # Класс наследуется от QWidget и яв
         self.buttonAddDiff = QPushButton('Add Diff', self)
         self.buttonAddDiff.clicked.connect(self.addDiff)
 
-        self.csv_table = QTableWidget()
-        self.gaussian_table = QTableWidget()
-        self.gaussian_table.setColumnCount(4)
-        self.gaussian_table.setHorizontalHeaderLabels(['Reaction', 'Height', 'Center', 'Width'])
-
-        self.stacked_widget = QStackedWidget()
-        self.stacked_widget.addWidget(self.csv_table)
-        self.stacked_widget.addWidget(self.gaussian_table)
-
         self.figure = Figure()
+        self.figure.set_size_inches(10, 5, forward=True)
         self.canvas = FigureCanvas(self.figure)
 
         self.buttonDeleteColumn = QPushButton('Delete X column', self)
         self.buttonDeleteColumn.clicked.connect(self.deleteColumn)
 
-        self.layout = QGridLayout()
+        # Создаем главный вертикальный лейаут
+        main_layout = QVBoxLayout()
+        
+        # Создаем горизонтальный лейаут для верхней области
+        top_layout = QHBoxLayout()
 
-        self.layout.addWidget(self.buttonLoadCSV, 0, 0)
-        self.layout.addWidget(self.buttonPlot, 1, 0)
-        self.layout.addWidget(self.buttonInteractive, 2, 0)
-        self.layout.addWidget(self.labelX, 0, 2)
-        self.layout.addWidget(self.comboBoxX, 0, 3)
-        self.layout.addWidget(self.labelY, 1, 2)
-        self.layout.addWidget(self.comboBoxY, 1, 3)
-        self.layout.addWidget(self.buttonAddDiff, 2, 2)
-        self.layout.addWidget(self.buttonDeleteColumn, 2, 3)
-        self.layout.addWidget(self.stacked_widget, 0, 4, 4, 1)
-        self.layout.addWidget(self.canvas, 4, 0, 1, 5)
+        # Добавляем кнопки, лейблы и выпадающие списки в верхнюю область
+        buttons_layout = QVBoxLayout()
+        buttons_layout.addWidget(self.buttonLoadCSV)
+        buttons_layout.addWidget(self.buttonPlot)
+        buttons_layout.addWidget(self.buttonInteractive)
+        buttons_layout.addWidget(self.labelX)
+        buttons_layout.addWidget(self.comboBoxX)
+        buttons_layout.addWidget(self.labelY)
+        buttons_layout.addWidget(self.comboBoxY)
+        buttons_layout.addWidget(self.buttonAddDiff)
+        buttons_layout.addWidget(self.buttonDeleteColumn)
 
-        self.setLayout(self.layout)
+        # Добавляем лейаут кнопок в верхний лейаут
+        top_layout.addLayout(buttons_layout)
+
+        # Добавляем таблицу в верхний лейаут
+        top_layout.addWidget(self.stacked_widget)
+
+        # Добавляем верхний лейаут в главный лейаут
+        main_layout.addLayout(top_layout)
+
+        # Добавляем холст для графика в главный лейаут
+        main_layout.addWidget(self.canvas)
+
+        self.setLayout(main_layout)
+
 
     def getCSV(self):
         self.viewer.getCSV()
@@ -75,58 +95,47 @@ class MainApp(QWidget): # Класс наследуется от QWidget и яв
         self.fillTable()
 
     def fillTable(self):
-        data = self.viewer.df
-        self.csv_table.setRowCount(data.shape[0])
-        self.csv_table.setColumnCount(data.shape[1])
-        self.csv_table.setHorizontalHeaderLabels(data.columns)
-        for i in range(data.shape[0]):
-            for j in range(data.shape[1]):
-                self.csv_table.setItem(i, j, QTableWidgetItem(str(data.iat[i, j])))
-
+        self.csv_model = PandasModel(self.viewer.df)
+        self.csv_table.setModel(self.csv_model)
 
     def fillComboBoxes(self):
-        # Эта функция заполняет выпадающие меню выбора столбцов для отрисовки графика.
-        # Функция использует загруженные данные (DataFrame) из класса CSVViewer.
         self.comboBoxX.clear()
         self.comboBoxY.clear()
         self.comboBoxX.addItems(self.viewer.df.columns)
         self.comboBoxY.addItems(self.viewer.df.columns)
 
     def interactiveMode(self, activated):
-        # Эта функция переключает приложение в интерактивный режим,
-        # где пользователь может рисовать гауссианы по графику.
         if activated:
-            self.stacked_widget.setCurrentIndex(1)  # Индекс Гауссианной таблицы
+            self.stacked_widget.setCurrentIndex(1)
             self.press_cid = self.canvas.mpl_connect('button_press_event', self.on_press)
             self.release_cid = self.canvas.mpl_connect('button_release_event', self.on_release)
         else:
-            self.stacked_widget.setCurrentIndex(0)  # Индекс таблицы CSV
+            self.stacked_widget.setCurrentIndex(0)
             self.canvas.mpl_disconnect(self.press_cid)
             self.canvas.mpl_disconnect(self.release_cid)
 
     def add_gaussian_to_table(self, height, center, width):
-        row_count = self.gaussian_table.rowCount()
-        self.gaussian_table.insertRow(row_count)
-        self.gaussian_table.setItem(row_count, 0, QTableWidgetItem(f'Reaction_{row_count + 1}'))
-        self.gaussian_table.setItem(row_count, 1, QTableWidgetItem(str(height)))
-        self.gaussian_table.setItem(row_count, 2, QTableWidgetItem(str(center)))
-        self.gaussian_table.setItem(row_count, 3, QTableWidgetItem(str(width)))
+        row_data = pd.DataFrame({'Reaction': [f'Reaction_{self.gaussian_data.shape[0] + 1}'], 
+                                'Height': [height], 'Center': [center], 'Width': [width]})
+        self.gaussian_data = pd.concat([self.gaussian_data, row_data], ignore_index=True)
+        
+        # обновляем модель таблицы
+        self.gaussian_model = PandasModel(self.gaussian_data)
+        self.gaussian_table.setModel(self.gaussian_model)
 
     def on_press(self, event):
-        # Эта функция вызывается при нажатии кнопки мыши на графике в интерактивном режиме.
         self.press_x = event.xdata
         self.press_y = event.ydata
-    
+
     def get_column_data(self, column_name):
         return self.viewer.df[column_name]
 
     def on_release(self, event):
-        # Эта функция вызывается при отпускании кнопки мыши на графике в интерактивном режиме.
         release_x = event.xdata
-        width = 2 * abs(release_x - self.press_x)  # Удвоенная ширина Гауссианы
+        width = 2 * abs(release_x - self.press_x)
         x_column_data = self.get_column_data(self.comboBoxX.currentText())
-        x = np.linspace(min(x_column_data), max(x_column_data), 1000)  # Диапазон значений x
-        y = self.gaussian(x, self.press_y, self.press_x, width)  # Значения Гауссианы
+        x = np.linspace(min(x_column_data), max(x_column_data), 1000)
+        y = self.gaussian(x, self.press_y, self.press_x, width)
 
         ax = self.figure.get_axes()[0]
         ax.plot(x, y, 'r-')
@@ -135,38 +144,37 @@ class MainApp(QWidget): # Класс наследуется от QWidget и яв
         self.add_gaussian_to_table(self.press_y, self.press_x, width)
 
     def gaussian(self, x, a0, a1, a2):
-        return a0 * np.exp(((-1/2) * ((x - a1)/a2)**2))
-          
-    def addDiff(self):
-        x_col_name = self.comboBoxX.currentText()
-        y_col_name = self.comboBoxY.currentText()
-        self.viewer.add_diff_column(x_col_name, y_col_name)
-        self.fillTable()
-        self.fillComboBoxes()
-    
-    def deleteColumn(self):
-        # Эта функция удаляет выбранный столбец из DataFrame.
-        col_name = self.comboBoxX.currentText()
-        if col_name in self.viewer.df.columns:
-            self.viewer.df = self.viewer.df.drop(columns=[col_name])
-            self.fillTable()
-            self.fillComboBoxes()
-    
-    def onclick(self, event):
-        # Эта функция вызывается при клике на график в интерактивном режиме.        
-        ax = self.figure.get_axes()[0]
-        ax.plot(event.xdata, event.ydata, 'ro')
-        self.canvas.draw()
+                return a0 * np.exp(-((x - a1) ** 2) / (2 * a2 ** 2))
 
     def plotGraph(self):
-        # Эта функция отрисовывает график на основе выбранных пользователем данных из ComboBox.
-        self.viewer.plotGraph(self.comboBoxX.currentText(), self.comboBoxY.currentText(), self.figure, self.canvas)
+        self.figure.clear()
+        
+        ax = self.figure.add_subplot(111)
+        ax.plot(self.viewer.df[self.comboBoxX.currentText()], self.viewer.df[self.comboBoxY.currentText()], 'b-')
+        
+        self.canvas.draw()
 
-def main():
-    app = QApplication(sys.argv)
-    main_app = MainApp()
-    main_app.show()
-    sys.exit(app.exec_())
+    def addDiff(self):
+        x_column = self.comboBoxX.currentText()
+        y_column = self.comboBoxY.currentText()
+        self.viewer.add_diff_column(x_column, y_column)
+        self.fillTable()
+        self.fillComboBoxes()
+
+    def deleteColumn(self):
+        if self.stacked_widget.currentIndex() == 0:
+            self.viewer.df.drop([self.comboBoxX.currentText()], axis=1, inplace=True)
+            self.fillTable()
+            self.fillComboBoxes()
+        elif self.stacked_widget.currentIndex() == 1:
+            if self.gaussian_table.currentIndex().row() >= 0:
+                self.gaussian_data.drop(self.gaussian_table.currentIndex().row(), inplace=True)
+                self.gaussian_data.reset_index(drop=True, inplace=True)
+                self.gaussian_model = PandasModel(self.gaussian_data)
+                self.gaussian_table.setModel(self.gaussian_model)
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MainApp()
+    ex.show()
+    sys.exit(app.exec_())
--- pandas_model.py
+++ pandas_model.py
@@ -1,30 +1,74 @@
-# pandas_model.py
-
 from PyQt5.QtCore import Qt, QAbstractTableModel
 
 class PandasModel(QAbstractTableModel):
+    """
+    Класс PandasModel, наследующий от QAbstractTableModel. Используется для взаимодействия 
+    между pandas DataFrame и QTableView.
+    """
+
     def __init__(self, data, parent=None):
+        """
+        Инициализатор класса.
+
+        :param data: pandas DataFrame, который будет отображаться.
+        :param parent: родительский виджет.
+        """
         QAbstractTableModel.__init__(self, parent)
         self._data = data
 
     def rowCount(self, parent=None):
+        """
+        Возвращает количество строк в DataFrame.
+
+        :param parent: родительский индекс (не используется, оставлен для совместимости).
+        :return: число строк.
+        """
         return self._data.shape[0]
 
     def columnCount(self, parent=None):
+        """
+        Возвращает количество столбцов в DataFrame.
+
+        :param parent: родительский индекс (не используется, оставлен для совместимости).
+        :return: число столбцов.
+        """
         return self._data.shape[1]
 
     def data(self, index, role=Qt.DisplayRole):
+        """
+        Возвращает данные для отображения в таблице.
+
+        :param index: индекс ячейки.
+        :param role: роль данных.
+        :return: данные для отображения в ячейке или None, если ячейка недействительна.
+        """
         if index.isValid():
             if role == Qt.DisplayRole:
                 return str(self._data.iloc[index.row(), index.column()])
         return None
 
     def headerData(self, col, orientation, role):
+        """
+        Возвращает заголовки для строк и столбцов.
+
+        :param col: индекс столбца.
+        :param orientation: ориентация заголовка (вертикальная или горизонтальная).
+        :param role: роль данных.
+        :return: заголовок столбца или None, если заголовок недействителен.
+        """
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self._data.columns[col]
         return None
 
     def setData(self, index, value, role):
+        """
+        Позволяет изменить данные в DataFrame.
+
+        :param index: индекс ячейки.
+        :param value: новое значение.
+        :param role: роль данных.
+        :return: True, если данные были успешно изменены, иначе False.
+        """
         if index.isValid():
             if role == Qt.EditRole:
                 row = index.row()
@@ -35,4 +79,10 @@ class PandasModel(QAbstractTableModel):
         return False
 
     def flags(self, index):
+        """
+        Определяет возможности ячейки.
+
+        :param index: индекс ячейки.
+        :return: набор флагов, определяющих возможности ячейки.
+        """
         return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
