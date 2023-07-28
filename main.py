816a4c802265c262302f05810bfa61d93e529c8c
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-07-28 16:18:33 +0300

    table_manager added

--- main.py
+++ main.py
@@ -5,58 +5,9 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from csv_viewer import CSVViewer
 from pandas_model import PandasModel
+from table_manager import TableManager
 import numpy as np
 import pandas as pd
-
-class TableManager:
-    """Класс для управления функциональностью таблиц."""
-    
-    def __init__(self, viewer):
-        """Инициализация класса."""
-        self.viewer = viewer
-        self.gaussian_data = pd.DataFrame(columns=['Reaction', 'Height', 'Center', 'Width'])
-        self.gaussian_model = PandasModel(self.gaussian_data)
-        self.gaussian_table = QTableView()
-        self.gaussian_table.setModel(self.gaussian_model)
-
-        self.csv_table = QTableView()
-
-        self.stacked_widget = QStackedWidget()
-        self.stacked_widget.addWidget(self.csv_table)
-        self.stacked_widget.addWidget(self.gaussian_table)
-
-    def fillTable(self):
-        """Заполнение таблицы данными."""
-        self.csv_model = PandasModel(self.viewer.df)
-        self.csv_table.setModel(self.csv_model)
-
-    def fillComboBoxes(self, comboBoxX, comboBoxY):
-        """Заполнение комбобоксов данными."""
-        comboBoxX.clear()
-        comboBoxY.clear()
-        comboBoxX.addItems(self.viewer.df.columns)
-        comboBoxY.addItems(self.viewer.df.columns)
-
-    def add_gaussian_to_table(self, height, center, width):
-        """Добавление гауссовской функции в таблицу."""
-        row_data = pd.DataFrame({'Reaction': [f'Reaction_{self.gaussian_data.shape[0] + 1}'], 
-                                'Height': [height], 'Center': [center], 'Width': [width]})
-        self.gaussian_data = pd.concat([self.gaussian_data, row_data], ignore_index=True)
-        self.gaussian_model = PandasModel(self.gaussian_data)
-        self.gaussian_table.setModel(self.gaussian_model)
-
-    def deleteColumn(self, comboBoxX, currentIndex):
-        """Удаление колонки из таблицы."""
-        if currentIndex == 0:
-            self.viewer.df.drop([comboBoxX.currentText()], axis=1, inplace=True)
-            self.fillTable()
-            self.fillComboBoxes(comboBoxX, comboBoxY)
-        elif currentIndex == 1:
-            if self.gaussian_table.currentIndex().row() >= 0:
-                self.gaussian_data.drop(self.gaussian_table.currentIndex().row(), inplace=True)
-                self.gaussian_data.reset_index(drop=True, inplace=True)
-                self.gaussian_model = PandasModel(self.gaussian_data)
-                self.gaussian_table.setModel(self.gaussian_model)
 
 class MainApp(QWidget): 
     """Главное приложение."""
@@ -193,7 +144,7 @@ class MainApp(QWidget):
 
     def deleteColumn(self):
         """Удаление колонки."""
-        self.tableManager.deleteColumn(self.comboBoxX, self.tableManager.stacked_widget.currentIndex())
+        self.tableManager.deleteColumn(self.comboBoxX, self.comboBoxY, self.tableManager.stacked_widget.currentIndex())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
